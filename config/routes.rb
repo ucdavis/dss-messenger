63f8f771c86c24edd909af01513f0ef6dbf3bf7f
Obada Kadri <abu3antar@gmail.com>
2013-02-07 16:16:53 -0800

    scaffolded Event model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DssMessenger::Application.routes.draw do
 
+  resources :events
+
+
   resources :impacted_services
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130208001413) do
+ActiveRecord::Schema.define(:version => 20130208001621) do
 
   create_table "classifications", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "events", :force => true do |t|
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
