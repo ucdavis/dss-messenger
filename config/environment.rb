8b1469c47ad51473428c0ce11ebd19679776c287
Obada Kadri <abu3antar@gmail.com>
2013-04-25 12:13:07 -0700

    Implemented CAS Authentication

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DssMessenger::Application.initialize!
+
+# Configure the CAS server
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url => "https://cas.ucdavis.edu/cas/"
+)
