6c740bd10f72436cdcc0deb0866db3ae0e361cf5
Obada Kadri <okadri@ucdavis.edu>
2013-07-22 10:43:15 -0700

    created unit test for Message

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save message with no subject" do
+    m = Message.new
+    m.impact_statement = "Some Impact Statement"
+    refute m.save, " |||||ERROR||||| Saved the message without subject"
+  end
+  test "Should not save message with no Impact Statement" do
+    m = Message.new
+    m.subject = "Some Subject"
+    refute m.save, " |||||ERROR||||| Saved the message without Impact Statement"
+  end
+  test "Should save message with a Subject & Impact Statement" do
+    m = Message.new
+    m.subject = "Some Subject"
+    m.impact_statement = "Some Impact Statement"
+    assert m.save, " |||||ERROR||||| Could not save the message with a Subject & Impact Statement"
+  end
+  test "Should find or create group recipient" do
+    message = Message.first
+    assert message.recipient_uids="57", " |||||ERROR||||| Could not find or create group recipient"
+  end
+  test "Should find or create individual recipient" do
+    message = Message.first
+    assert message.recipient_uids="13907", " |||||ERROR||||| Could not find or create individual recipient"
+  end
+  test "Should send email to group recipients" do
+    message = Message.first
+    message.recipient_uids="57"
+    assert message.send_mass_email, " |||||ERROR||||| Could not send email to group recipients"
+  end
+  test "Should send email to individual recipient" do
+    message = Message.first
+    message.recipient_uids="13907"
+    assert message.send_mass_email, " |||||ERROR||||| Could not send email to individual recipient"
+  end
 end
