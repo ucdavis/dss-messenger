831c7d1da779ae945cec70ce62393ca6c16d2ddc
Obada Kadri <okadri@ucdavis.edu>
2013-08-07 14:44:12 -0700

    added footer to the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ require 'active_record/fixtures'
 ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "classifications")
 ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "modifiers")
 ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "impacted_services")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "settings")
