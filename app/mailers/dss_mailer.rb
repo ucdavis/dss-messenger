043fd8c99a910e79567e75495e4aae41cd9901b1
Obada Kadri <okadri@ucdavis.edu>
2013-08-16 16:10:44 -0700

    Changed sender back to logged in user, until we get whitelisted

--- app/mailers/dss_mailer.rb
+++ app/mailers/dss_mailer.rb
@@ -5,11 +5,11 @@ class DssMailer < ActionMailer::Base
     @message = message
     @member = member
     @footer = Setting.where(:item_name => 'footer').first.item_value
-    # @sender = Person.find(message.sender_uid)
+    @sender = Person.find(message.sender_uid)
     modifier = @message.modifier.description.slice(0..(@message.modifier.description.index(':'))) if @message.modifier
     classification = @message.classification.description.slice(0..(@message.classification.description.index(':'))) if @message.classification
     subject = "#{modifier} #{classification} #{@message.subject}"
-    mail(:to => "#{@member.name} <#{@member.email}>", :subject => subject)
-    # mail(:from => "#{@from_name} <#{@from_email}>", :to => "#{@member.name} <#{@member.email}>", :subject => subject)
+    # mail(:to => "#{@member.name} <#{@member.email}>", :subject => subject)
+    mail(:from => "#{@from_name} <#{@from_email}>", :to => "#{@member.name} <#{@member.email}>", :subject => subject)
   end
 end
