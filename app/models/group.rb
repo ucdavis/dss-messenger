482fd351b224bae0c8b8ad905acb9c629f0a349b
Obada Kadri <okadri@ucdavis.edu>
2013-08-22 10:13:53 -0700

    Implemented RM API v1 URL

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ActiveResource::Base
   self.site = $DSS_RM_SETTINGS['HOST']
   self.user = $DSS_RM_SETTINGS['USER']
   self.password = $DSS_RM_SETTINGS['PASSWORD']
+  headers['Accept'] = "application/vnd.roles-management.v1"
 end
