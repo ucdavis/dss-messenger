77bd4f861d5df95b9e916ef8a3e878b8da60edb0
Christopher Thielen <cthielen@gmail.com>
2013-08-28 11:05:30 -0700

    Added proper credit for email logger code

--- config/initializers/email_logger.rb
+++ config/initializers/email_logger.rb
@@ -1,3 +1,5 @@
+# Credit: http://trevorturk.com/2011/02/25/logging-outgoing-emails-in-rails-3/
+
 class EmailLogger
   def self.delivered_email(email)
     Rails.logger.tagged("email") { Rails.logger.info "Message sent from '#{email.from}' to '#{email.to}' with subject '#{email.subject}'" }
