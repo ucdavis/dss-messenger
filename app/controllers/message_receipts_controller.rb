75939f8393988be4ba3b2a2206804db65707bcff
Eric Lin <eric@ericflin.com>
2015-06-18 16:42:03 -0700

    Bug fix for callbacks that don't return a Proc

--- app/controllers/message_receipts_controller.rb
+++ app/controllers/message_receipts_controller.rb
@@ -6,7 +6,7 @@ class MessageReceiptsController < ApplicationController
   def show
     message_receipt = MessageReceipt.find_by_id(params[:id])
     callback_response = message_receipt.message_log.publisher.class_name.constantize.callback(params[:id])
-    callback_response.call(self)
+    callback_response.call(self)  if callback_response.methods.include? :call
     self.response_body = ""  unless performed?
   end
 end
