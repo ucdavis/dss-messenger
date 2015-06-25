72d5650156f6821af81752a85029bab0a3b40180
Eric Lin <eric@ericflin.com>
2015-06-25 15:31:28 -0700

    Added Publishers tab to Preferences dialog box, to allow changing publishers' names and setting default publishers

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -9,4 +9,16 @@ class PublishersController < ApplicationController
       format.json { render json: @publishers }
     end
   end
+
+  def update
+    @publisher = Publisher.find(params[:id])
+
+    respond_to do |format|
+      if @publisher.update_attributes(params[:publishers])
+        format.json { head :no_content }
+      else
+        format.json { render json: @publisher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
