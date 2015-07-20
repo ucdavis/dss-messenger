1819e2a8292c08c0a87c501c97d63953361232e3
Christopher Thielen <cmthielen@ucdavis.edu>
2015-07-20 12:29:25 -0700

    Rescan publishers on application start. Fixes #58.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,15 @@
 require 'active_record/fixtures'
 
-unless Classification.exists?
-  ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "classifications")
-end
-unless Modifier.exists?
-  ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "modifiers")
-end
-unless ImpactedService.exists?
-  ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "impacted_services")
-end
-unless Setting.exists?
-  ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "settings")
-end
-
-# Scan app/publisher directory for Publishers and add them to the database
-Dir.entries(Rails.root + "app/publishers")
-.map do |x|
-  unless x.start_with?(".") || File.directory?(x) || ! x.end_with?(".rb")
-    class_file = File.open(Rails.root + "app/publishers/" + x)
-    until class_file.eof()
-      class_line = class_file.readline()
-      break class_line.gsub(/.* (.*) < Publisher/, '\1').strip  if class_line.include? "< Publisher"
-      break if class_line.start_with? "class"
-    end
-  end
-end
-.delete_if { |x| x.nil? }
-.uniq
-.map do |publisher|
-  # Don't overwrite/re-add previously added publishers
-  if Publisher.where(class_name: publisher).first.nil?
-    new_publisher = Publisher.new
-    new_publisher.class_name = publisher
-    new_publisher.name = publisher.underscore.humanize.titleize.gsub(/ Publisher$/, '').strip
-    new_publisher.save!
-  end
-end
+# Not sure it's a good idea to mingle test data with real data. Maybe.
+# unless Classification.exists?
+#   ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "classifications")
+# end
+# unless Modifier.exists?
+#   ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "modifiers")
+# end
+# unless ImpactedService.exists?
+#   ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "impacted_services")
+# end
+# unless Setting.exists?
+#   ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "settings")
+# end
