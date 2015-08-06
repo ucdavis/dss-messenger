95e468b0c4a60acaec3d6c6ddcd9321604a8c0cf
Christopher Thielen <cmthielen@ucdavis.edu>
2015-08-06 15:05:05 -0700

    Whitespace clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,18 @@
 DssMessenger::Application.routes.draw do
 
   resources :message_receipts
-
-
   resources :settings
-
-
   resources :impacted_services
-
-
   resources :classifications
-
-
   resources :modifiers
-
-
   resources :recipients
-
   resources :publishers
+  resources :messages
 
   get "/messages/open" => 'messages#open'
-  resources :messages
-  
-  root :to => 'messages#index'
-
   get "/logout" => 'application#logout'
   get "/status" => 'application#status'
   get "/delayed_job_status" => 'delayed_job_status#index'
+
+  root :to => 'messages#index'
 end
