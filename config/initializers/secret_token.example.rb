f4cd400b456c2a7ea52b1d2a97bd16768dd43f23
Christopher Thielen <cmthielen@ucdavis.edu>
2015-08-14 15:56:16 -0700

    Update to Rails 4 config.secret_key_base syntax.

--- config/initializers/secret_token.example.rb
+++ config/initializers/secret_token.example.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DssMessenger::Application.config.secret_token = 'SECRET_TOKEN'
+DssMessenger::Application.config.secret_key_base = 'Put the results of running "rake token" here.'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DssMessenger::Application.config.secret_token = 'd1e6fcb409c885a8ce32ad540f7ebd10e1dbbbd4faca70d8961484df549d1346355cbd794b0b2f41e4f6639d663dbc138a31af350bf58fa0f835a8f2a28992b2'
+DssMessenger::Application.config.secret_key_base = 'd1e6fcb409c885a8ce32ad540f7ebd10e1dbbbd4faca70d8961484df549d1346355cbd794b0b2f41e4f6639d663dbc138a31af350bf58fa0f835a8f2a28992b2'
