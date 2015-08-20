f32e648a680a47e5e95c5c73a5fa86cfe0fdcf97
Christopher Thielen <cmthielen@ucdavis.edu>
2015-08-20 12:03:32 -0700

    Add helper methods to calculate short_description.

--- app/models/modifier.rb
+++ app/models/modifier.rb
@@ -2,4 +2,12 @@ class Modifier < ActiveRecord::Base
   has_many :messages
 
   validates :description, presence: true
+
+  def short_description
+    if self.description and self.description.index(':')
+      return self.description.slice(0..(self.description.index(':') - 1))
+    else
+      return self.description
+    end
+  end
 end
