648bcfd9d0d3c5e4c3c54822ee6cee0eb1fbb916
Christopher Thielen <cmthielen@ucdavis.edu>
2015-08-26 10:36:18 -0700

    Added some documentation on the nature of MessageLog

--- app/models/message_log.rb
+++ app/models/message_log.rb
@@ -1,3 +1,9 @@
+# A MessageLog object should exist for each message+publisher combination.
+# For example, a message only sent via e-mail will have a single MessageLog.
+# Another message sent via e-mail and AggieFeed will have two MessageLog objects.
+# Each MessageLog object tracks its own unique send receipts.
+# MessageLog 'entries' may not be correctly filled in while status is 'queued'
+# as the background bulk_send task still needs to set up the data.
 class MessageLog < ActiveRecord::Base
   STATUSES = [:queued, :sending, :completed, :error]
 
