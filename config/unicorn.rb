ead9e06554db1a44d487f2e5570eea4b44e30f4f
Obada Kadri <okadri@ucdavis.edu>
2016-04-19 16:20:03 -0700

    Updates nginx.conf file to redirect to https

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,8 @@ working_directory app_dir
 
 pid "#{app_dir}/tmp/unicorn.pid"
 
-# stderr_path "#{app_dir}/log/unicorn.stderr.log"
-# stdout_path "#{app_dir}/log/unicorn.stdout.log"
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 worker_processes 1
 listen "/tmp/unicorn.sock", :backlog => 64
