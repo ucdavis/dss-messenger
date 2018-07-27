893d2808032a1015e12ebedc3a4edff3ce019572
Christopher Thielen <cmthielen@ucdavis.edu>
2018-07-27 12:20:59 -0700

    Minor bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dss_messenger_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -81,7 +76,8 @@ Rails.application.configure do
     domain:                Rails.application.secrets.smtp_domain,
     user_name:             Rails.application.secrets.smtp_username,
     password:              Rails.application.secrets.smtp_password,
-    enable_starttls_auto:  true
+    enable_starttls_auto:  true,
+    logger:                Rails.logger
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
