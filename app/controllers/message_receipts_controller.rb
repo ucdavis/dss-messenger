1f3c5ad918835fadf5ee486b82a393eaec565dce
Christopher Thielen <cmthielen@ucdavis.edu>
2018-07-31 11:51:25 -0700

    Address NPE in MessageReceipts#show

--- app/controllers/message_receipts_controller.rb
+++ app/controllers/message_receipts_controller.rb
@@ -1,7 +1,14 @@
 class MessageReceiptsController < ApplicationController
+  before_action :set_message_receipt, only: [:show]
+
   def show
-    message_receipt = MessageReceipt.find_by_id(params[:id])
-    callback_response = message_receipt.message_log.publisher.classify.callback(params[:id], self)
+    @message_receipt&.message_log&.publisher&.classify&.callback(params[:id], self)
     self.response_body = '' unless performed?
+  end
+
+  private
+
+  def set_message_receipt
+    @message_receipt = MessageReceipt.find_by_id(params[:id]) if params[:id]
   end
 end
