93fb840442d4b9efd01caab9ad90c5850cb0e719
Christopher Thielen <cmthielen@ucdavis.edu>
2018-08-07 08:47:01 -0700

    Handle invalid authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
-  before_action :authenticate, except: [:open, :show, :status, :error_404]
+  before_action :authenticate, except: [:open, :show, :status, :error_404, :bad_request]
   protect_from_forgery with: :exception
+
+  rescue_from ActionController::UnknownFormat, with: :bad_request
+  rescue_from ActionController::InvalidAuthenticityToken, with: :bad_request
 
   def logout
     CASClient::Frameworks::Rails::Filter.logout(self)
@@ -15,5 +18,11 @@ class ApplicationController < ActionController::Base
   def error_404
     @requested_path = request.path
     render plain: "No such path #{@requested_path}", status: 404
+  end
+
+  protected
+
+  def bad_request
+    render plain: 'Bad request', status: 400
   end
 end
