735d533eb6bb8b351efbbec2e989f6d59e5b4ec1
Jarold Wong <cwjwong@ucdavis.edu>
2026-04-28 13:25:01 -0700

    fix: name for autoload

--- app/publishers/rss_publisher.rb
+++ app/publishers/rss_publisher.rb
@@ -1,7 +1,7 @@
 # +RSSPublisher+ doesn't actually do anything. The controller action that
 # displays the RSS feed only shows messages that have an RSS publisher
 # attached.
-class RSSPublisher < Publisher
+class RssPublisher < Publisher
   # Modifies +message_log+ to say that sending is completed.
   def self.schedule(message_log, recipient_list)
     message_log.status = :completed
